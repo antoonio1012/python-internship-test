368089ac8bb9805ef0f8dec070cdc2dceab85b49
antoonio1012 <antoniojoshua1015@gmail.com>
2025-11-27 09:44:23 +0700

    Project IHT

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Project IHT', 
+    'version': '1.0',
+    'summary': 'Modul Custom Inheritance Project',
+    'depends': ['project'], 
+    'data': [
+        'views/project_views.xml',
+    ],
+    'application': True,
+}
---
+++ models/project.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+class Project(models.Model):
+    _inherit = 'project.project' 
+    lokasi_proyek = fields.Text(string="Lokasi Proyek")
+    source_aplikasi_pendukung = fields.Char(string="Source Aplikasi Pendukung")
+    daftar_perkerja_remote = fields.Json(string="Daftar Pekerja Remote")
